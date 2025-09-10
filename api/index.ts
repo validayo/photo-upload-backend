af7092c67ca4afdc6f357ee7d19d091c6f0fdce2
validayo <ayodejiadigun06@gmail.com>
2025-09-10 14:58:18 -0400

    updated export default

--- api/index.ts
+++ api/index.ts
@@ -50,10 +50,10 @@ app.get("/health", (_, res) => {
 console.log("Loaded router file: <filename>");
 
 // Mount routers
-app.use("/contact-form", contactFormRouter);
-app.use("/newsletter", newsletterRouter);
-app.use("/upload-photos", storageRouter);
-app.use("/images", galleryRouter);
+app.use("/*contact-form", contactFormRouter);
+app.use("/*newsletter", newsletterRouter);
+app.use("/*upload-photos", storageRouter);
+app.use("/*images", galleryRouter);
 
 // Error handler (with CORS)
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
@@ -64,5 +64,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(err.status || 500).json({ error: err.message });
   }
 });
+
 
 export default app;
